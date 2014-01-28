8be3c87bda7671a001ee0fb652282998bcc989da
xmig <xmig@yandex.ru>
2014-01-28 04:24:03 +0200

    fibomacci recursion update

--- fibonnachi.py
+++ fibonnachi.py
@@ -1,3 +1,14 @@
+"""
+Please enter a short description here. It could be useful:
+ - for getting "a good habit"
+ - english training
+ - it is useful for studding too
+
+"""
+
+
+#just a note
+# ';' are extra un Python
 def fibonacci_recursion(n, num=0):
     if n >= 2:
         num = fibonacci_recursion(n - 1) + fibonacci_recursion(n - 2);
@@ -6,6 +17,19 @@ def fibonacci_recursion(n, num=0):
     return num;
 
 print("Element under the number N in the Fibonacci series: ", fibonacci_recursion(8));
+
+def fb(n, val=[1, 1]):
+    """
+    fibonacci_recursion 2
+    val=[1, 1]  - warning is OK here
+    """
+    if n <= 0:
+        return val
+    val.append(val[-1] + val[-2])
+    return fb(n - 1, val)
+
+print fb(15)
+
 
 def fib(n):
 
@@ -28,6 +52,10 @@ print(fib_list(200))
 
 
 def word_count(sentence):
+    """
+    Works correct.
+    But very much complicated in implementation
+    """
     sentence_new = ' '.join(sentence.split())
     index_gaps = []
     n = sentence.strip().split(" ")
@@ -37,7 +65,7 @@ def word_count(sentence):
             index_gaps.append(n)
     return len(index_gaps) + 1
 
-print(word_count("      The best   bbb             "))
+print(word_count("      The best   bbb       yy      1"))
 print(word_count("          The          best picture          I've ever seen"))
 
 
