e60eadffdef54d5db65e71f7b6ed89b36d82615b
stretiak <sergii.tretiak.21@gmail.com>
2014-02-27 10:40:47 +0200

    Update copy.py

--- copy.py
+++ copy.py
@@ -9,6 +9,10 @@ def  increase(sentence, number_of_items):
 #this is onle the test line for commit
 #HE-heeeeeee
 
+
+#WWWWWWW
+
+
 #increase("The best picture I've ever seen (copy)", 5)
 
 def find_increase(sentence):
